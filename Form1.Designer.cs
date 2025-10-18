1d0dcf8fbe4348193179d4b9d891bc9e920d4f16
Misha <mischikus@gmail.com>
2025-10-18 17:49:01 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace AutoRiaAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.cbBrand = new System.Windows.Forms.ComboBox();
+            this.cbModel = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataGridCars = new System.Windows.Forms.DataGridView();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.chartCars = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Navy;
+            this.lblTitle.Location = new System.Drawing.Point(28, 21);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(297, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Анализатор цен AUTO.RIA";
+            // 
+            // cbBrand
+            // 
+            this.cbBrand.FormattingEnabled = true;
+            this.cbBrand.Location = new System.Drawing.Point(35, 78);
+            this.cbBrand.Name = "cbBrand";
+            this.cbBrand.Size = new System.Drawing.Size(121, 21);
+            this.cbBrand.TabIndex = 1;
+            this.cbBrand.Text = "Выбор марки авто";
+            this.cbBrand.SelectedIndexChanged += new System.EventHandler(this.cmbBrand_SelectedIndexChanged);
+            // 
+            // cbModel
+            // 
+            this.cbModel.FormattingEnabled = true;
+            this.cbModel.Location = new System.Drawing.Point(200, 78);
+            this.cbModel.Name = "cbModel";
+            this.cbModel.Size = new System.Drawing.Size(121, 21);
+            this.cbModel.TabIndex = 2;
+            this.cbModel.Text = "Выбор модели";
+            this.cbModel.SelectedIndexChanged += new System.EventHandler(this.cmbModel_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.ForeColor = System.Drawing.Color.White;
+            this.btnLoad.Location = new System.Drawing.Point(104, 105);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(146, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Загрузить данные";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // dataGridCars
+            // 
+            this.dataGridCars.AllowUserToAddRows = false;
+            this.dataGridCars.AllowUserToDeleteRows = false;
+            this.dataGridCars.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridCars.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridCars.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridCars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCars.GridColor = System.Drawing.Color.LightGray;
+            this.dataGridCars.Location = new System.Drawing.Point(40, 160);
+            this.dataGridCars.Name = "dataGridCars";
+            this.dataGridCars.ReadOnly = true;
+            this.dataGridCars.RowHeadersVisible = false;
+            this.dataGridCars.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridCars.Size = new System.Drawing.Size(240, 150);
+            this.dataGridCars.TabIndex = 4;
+            // 
+            // lblStats
+            // 
+            this.lblStats.AutoSize = true;
+            this.lblStats.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lblStats.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStats.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblStats.Location = new System.Drawing.Point(391, 65);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(167, 63);
+            this.lblStats.TabIndex = 5;
+            this.lblStats.Text = "Средняя цена:\r\nДиапазон: min–max\r\nГрафик цен →\r\n";
+            // 
+            // chartCars
+            // 
+            this.chartCars.BackColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.Name = "ChartArea1";
+            this.chartCars.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartCars.Legends.Add(legend1);
+            this.chartCars.Location = new System.Drawing.Point(317, 160);
+            this.chartCars.Name = "chartCars";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartCars.Series.Add(series1);
+            this.chartCars.Size = new System.Drawing.Size(261, 231);
+            this.chartCars.TabIndex = 6;
+            this.chartCars.Text = "chart1";
+            this.chartCars.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(590, 424);
+            this.Controls.Add(this.chartCars);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.dataGridCars);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.cbModel);
+            this.Controls.Add(this.cbBrand);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Price";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCars)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ComboBox cbBrand;
+        private System.Windows.Forms.ComboBox cbModel;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dataGridCars;
+        private System.Windows.Forms.Label lblStats;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartCars;
+        //private System.Windows.Forms.DataVisualization.Charting.Chart chartPrices;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace AutoRiaAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, Dictionary<string, List<int>>> carData =
+            new Dictionary<string, Dictionary<string, List<int>>>
+            {
+                ["Toyota"] = new Dictionary<string, List<int>>
+                {
+                    ["Corolla"] = new List<int> { 12000, 12500, 13000, 11800, 14000 },
+                    ["Camry"] = new List<int> { 18000, 19000, 20000, 17500, 21000 },
+                    ["RAV4"] = new List<int> { 23000, 24000, 24500, 25000, 25500 }
+                },
+                ["BMW"] = new Dictionary<string, List<int>>
+                {
+                    ["3 Series"] = new List<int> { 22000, 23000, 21000, 23500, 24000 },
+                    ["5 Series"] = new List<int> { 30000, 32000, 31000, 29500, 31500 },
+                    ["X5"] = new List<int> { 40000, 42000, 39000, 41000, 43000 }
+                },
+                ["Audi"] = new Dictionary<string, List<int>>
+                {
+                    ["A3"] = new List<int> { 19000, 19500, 20000, 18500, 21000 },
+                    ["A4"] = new List<int> { 25000, 26000, 25500, 24500, 27000 },
+                    ["Q5"] = new List<int> { 35000, 34000, 36000, 37000, 35500 }
+                },
+                ["Mercedes"] = new Dictionary<string, List<int>>
+                {
+                    ["C-Class"] = new List<int> { 26000, 27000, 25500, 26500, 27500 },
+                    ["E-Class"] = new List<int> { 32000, 33000, 34000, 31500, 33500 },
+                    ["GLC"] = new List<int> { 40000, 42000, 41000, 39500, 42500 }
+                },
+                ["Volkswagen"] = new Dictionary<string, List<int>>
+                {
+                    ["Golf"] = new List<int> { 15000, 16000, 15500, 14500, 15800 },
+                    ["Passat"] = new List<int> { 20000, 21000, 19500, 20500, 21500 },
+                    ["Tiguan"] = new List<int> { 26000, 27000, 26500, 25500, 27500 }
+                },
+                ["Mazda"] = new Dictionary<string, List<int>>
+                {
+                    ["Mazda3"] = new List<int> { 15000, 15500, 14800, 16000, 16200 },
+                    ["CX-5"] = new List<int> { 24000, 24500, 23800, 25000, 25500 },
+                    ["CX-9"] = new List<int> { 32000, 33000, 32500, 33500, 34000 }
+                }
+            };
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadBrands();
+        }
+
+        private void UpdateDataGrid()
+        {
+            if (cbBrand.SelectedItem == null || cbModel.SelectedItem == null)
+                return;
+
+            string brand = cbBrand.SelectedItem.ToString();
+            string model = cbModel.SelectedItem.ToString();
+
+            var prices = carData[brand][model];
+
+            // Создаем список для DataGrid
+            List<CarEntry> gridData = new List<CarEntry>();
+            int i = 1;
+            foreach (var price in prices)
+            {
+                gridData.Add(new CarEntry
+                {
+                    Brand = brand,
+                    Model = model,
+                    Price = price
+                });
+                i++;
+            }
+
+            // Привязка к DataGridView
+            dataGridCars.DataSource = gridData;
+        }
+
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // Можно что-то добавить сюда, например:
+            // MessageBox.Show("Щёлкнули по графику!");
+        }
+
+
+        private void LoadBrands()
+        {
+            cbBrand.Items.Clear();
+            cbBrand.Items.AddRange(carData.Keys.ToArray());
+        }
+
+        private void cmbBrand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string brand = cbBrand.SelectedItem.ToString();
+            cbModel.Items.Clear();
+            cbModel.Items.AddRange(carData[brand].Keys.ToArray());
+            cbModel.SelectedIndex = 0;
+        }
+
+        private void cmbModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateChart();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateChart();
+            UpdateDataGrid();
+        }
+
+        private void UpdateChart()
+        {
+            if (cbBrand.SelectedItem == null || cbModel.SelectedItem == null)
+                return;
+
+            string brand = cbBrand.SelectedItem.ToString();
+            string model = cbModel.SelectedItem.ToString();
+            var prices = carData[brand][model];
+
+            chartCars.Series.Clear();
+            var series = new Series($"{brand} {model}")
+            {
+                ChartType = SeriesChartType.Column,
+                Color = System.Drawing.Color.SteelBlue
+            };
+
+            int i = 1;
+            foreach (var price in prices)
+                series.Points.AddXY($"Объявление {i++}", price);
+
+            chartCars.Series.Add(series);
+
+            double avg = prices.Average();
+            lblStats.Text = $"Средняя цена: {avg:F0} $";
+        }
+    }
+    public class CarEntry
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+
+}
